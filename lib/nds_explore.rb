06390768b42d0d99276b022de19e80c5ad3e839a
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-16 21:34:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors'
+require 'directors_database'
+  require 'pp'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
